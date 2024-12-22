7be4eae7e768ffeda3d3ae160ed59716e0579441
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-21 23:36:10 -0800

    Refactor Header Component for Simplified Design
    - Removed the logo text link to streamline the header design.
    - Adjusted padding for a more compact layout.
    - Retained essential navigation links and the `ModeToggle` component.
    - Simplified overall header structure for a cleaner and focused appearance.

--- components/header.tsx
+++ components/header.tsx
@@ -4,10 +4,10 @@ import { ModeToggle } from './mode-toggle'
 export default function Header() {
   return (
     <header className="border-b">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">
-          <span className="text-primary">Project</span>Setup
-        </Link>
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        {/* <Link href="/" className="text-2xl font-bold">
+          <span className="text-primary">MVP </span>Setup
+        </Link> */}
         <nav className="flex items-center space-x-4">
           <Link href="/docs" className="text-sm font-medium hover:underline">
             Docs
