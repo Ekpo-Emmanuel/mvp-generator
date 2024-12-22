777cf0f6f63d22f48e98c1eb42bcda17ba551825
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-21 23:37:15 -0800

    removed header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
-import Header from '@/components/header'
 import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,7 +20,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col min-h-screen">
-            <Header />
             <main className="flex-grow">{children}</main>
             <Footer />
           </div>
