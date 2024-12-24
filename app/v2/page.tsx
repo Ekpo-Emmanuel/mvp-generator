005434d7b96ca541510abbcff15cf5f6fb9e1c74
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-23 20:55:40 -0800

    refactor: Update Home2 page to include Header and StepsWrapper components

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -1,34 +1,19 @@
 'use client'
 
+import Header from '@/components/v2/header'
 import StepsWrapper from '@/components/v2/steps-wrapper'
-import ModeToggle from '@/components/mode-toggle'
 
 export default function Home2() {
   return (
-    <div className="bg-background text-foreground">
-      <div className="max-w-4xl mx-auto space-y-10 h-full">
-        <div className="flex items-center justify-between border-b px-4 py-2 lg:px-0">
-          <div className="flex items-end gap-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              id="logo-72"
-              width="22"
-              fill="none"
-              viewBox="0 0 53 44"
-              className="text-primary dark:text-primary"
-            >
-              <path
-                d="m23.3 0 28.746 28.63V44H38.631v-9.845L17.752 13.361h-4.337V44H0V0zM38.63 15.27V0h13.415v15.27z"
-                className="fill-current"
-              />
-            </svg>
+    <section className="bg-background">
+      <Header />
+      <div className="text-foreground pt-24">
+        <div className="max-w-4xl mx-auto space-y-10 h-full">
+          <div className="p-4 lg:px-0">
+            <StepsWrapper />
           </div>
-          <ModeToggle />
-        </div>
-        <div className="p-4 lg:px-0">
-          <StepsWrapper />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
