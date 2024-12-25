a4239ca8fcde48d65e52f795e0d77a2901e10308
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-25 09:49:50 -0800

    refactor: Update Home2 page to include Header and StepsWrapper components

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -1,19 +1,17 @@
-'use client'
+'use client';
 
-import Header from '@/components/v2/header'
-import StepsWrapper from '@/components/v2/steps-wrapper'
+import Header from '@/components/v2/header';
+import StepsWrapper from '@/components/v2/steps-wrapper';
 
 export default function Home2() {
   return (
-    <section className="bg-background">
+    <section className="bg-background relative">
       <Header />
-      <div className="text-foreground pt-24">
-        <div className="max-w-4xl mx-auto space-y-10 h-full">
-          <div className="p-4 lg:px-0">
-            <StepsWrapper />
-          </div>
+      <div className="text-foreground">
+        <div className="max-w-4xl mx-auto h-full p-4 lg:p-0 flex items-center justify-center">
+          <StepsWrapper />
         </div>
       </div>
     </section>
-  )
+  );
 }
