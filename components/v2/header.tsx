e28e5b2e947143993c02fa6d46dbcd94dd8adebf
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-25 09:50:22 -0800

    refactor: Update Header component to use V2LogoSvg for improved branding and navigation

--- components/v2/header.tsx
+++ components/v2/header.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 import ModeToggle from "@/components/mode-toggle";
+import V2LogoSvg from "./svgs/v2-logo";
 
 export default function Header() {
   return (
     <header className="fixed top-0 w-full bg-background z-50">
         <div className="flex items-center justify-between border-b px-4 py-2 lg:px-0 max-w-4xl mx-auto">
-        <div className="flex items-end gap-1">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            id="logo-72"
-            width="22"
-            fill="none"
-            viewBox="0 0 53 44"
-            className="text-primary dark:text-primary"
-            >
-            <path
-                d="m23.3 0 28.746 28.63V44H38.631v-9.845L17.752 13.361h-4.337V44H0V0zM38.63 15.27V0h13.415v15.27z"
-                className="fill-current"
-            />
-            </svg>
-        </div>
+        <V2LogoSvg />
         <ModeToggle />
         </div>
     </header>
